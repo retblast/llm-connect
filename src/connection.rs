dfc2adc40341a4b39a8365aafa1a7c083c7ea195
retblast <retblast@proton.me>
2026-04-01 12:04:11 -0500

    Added dependencies and features - deps: anyhow, futures-util, reqwest, sysinfo - added koboldcpp in tts (qwen3-tts) support
    Signed-off-by: retblast <retblast@proton.me>

--- src/connection.rs
+++ src/connection.rs
@@ -1,5 +1,11 @@
 // All about connections
-use std::process::Command;
+use anyhow::{Context, Result};
+use futures_util::StreamExt;
+use std::io::Write;
+use std::thread::{self, sleep};
+use std::time::Duration;
+use std::{fs::File, process::Command};
+use sysinfo::{ProcessRefreshKind, RefreshKind, System};
 
 #[derive(serde::Serialize, serde::Deserialize)]
 pub struct Message {
@@ -22,6 +28,13 @@ pub struct LlamaResponseChoices {
 #[derive(serde::Deserialize)]
 pub struct LlamaResponse {
     pub choices: Vec<LlamaResponseChoices>,
+}
+
+#[derive(serde::Serialize)]
+struct KoboldTTSRequest {
+    model: String,
+    input: String,
+    voice: String,
 }
 
 fn build_llama_prompt(
@@ -86,36 +99,128 @@ fn koboldcpp_configure_tts(
     return tts_command;
 }
 
+pub async fn check_llm_alive_yet(host: &String, port: &u32, retries: &u8) -> bool {
+    let client = reqwest::Client::new();
+    let mut response = client.get(format!("http://{host}:{port}")).send().await;
+    let mut response_code: u16 = 0;
+    let mut our_retries = retries.clone();
+    while our_retries > 0 {
+        sleep(Duration::new(1, 0));
+        our_retries -= 1;
+        response = client.get(format!("http://{host}:{port}")).send().await;
+        response_code = match response {
+            Ok(response_result) => response_result.status().as_u16(),
+            Err(_) => 0,
+        };
+    }
+    match response_code {
+        200 => true,
+        _ => false,
+    }
+}
 // Returns the PID of the running koboldcpp instance
-pub fn koboldcpp_start(mode: &String, model_dir: &String, voice_refs_dir: &String) -> u32 {
+pub async fn koboldcpp_start(
+    mode: &String,
+    host: &String,
+    port: &u32,
+    model_dir: &String,
+    voice_refs_dir: &String,
+) -> Result<u32, Box<dyn std::error::Error>> {
     let mut main_command = Command::new("koboldcpp");
     main_command
+        .arg("--host")
+        .arg(format!("{host}"))
+        .arg("--port")
+        .arg(format!("{port}"))
         .arg("--gpulayers")
         .arg("-1")
         .arg("--threads")
         .arg("16")
         .arg("--usevulkan");
+
+    // Just to have it initialized
     let mut final_command = Command::new("ls");
     match mode.as_str() {
         "tts" => final_command = koboldcpp_configure_tts(model_dir, voice_refs_dir, main_command),
         &_ => println!("Whoops @ koboldcpp_start"),
     }
-    println!("{:?}", final_command);
-    final_command.spawn().expect("Starting koboldcpp failed");
+    // KoboldCPP puts initialization details here, and its last line includes where the http api lies
+    let stdout_file = File::create("koboldcpp_stdout.txt")?;
+    // And it puts here details about the generation operations
+    let stderr_file = File::create("koboldcpp_stderr.txt")?;
 
-    //TODO: Get the PID
-    return 0;
+    // TODO: Make this print only by a flag
+    //println!("{:?}", final_command);
+    //
+    final_command.stdout(stdout_file);
+    final_command.stderr(stderr_file);
+    let koboldcpp_process = final_command.spawn()?;
+
+    if !check_llm_alive_yet(host, port, &10_u8).await {
+        process_killer(&koboldcpp_process.id());
+        panic!("koboldcpp took too long to start!");
+    };
+    //TODO: Having the PID is good for cleanup, but we have to check that it is up already
+    return Ok(koboldcpp_process.id());
 }
 
-fn koboldcpp_build_prompt(
-    mode: &String,
-    system_prompt: &String,
-    user_prompt: &String,
-    temperature: &f32,
-    max_tokens: &u32,
-) {
+fn koboldcpp_tts_build_prompt(model: &String, input: &String, voice: &String) -> KoboldTTSRequest {
+    let request = KoboldTTSRequest {
+        model: model.clone(),
+        input: input.clone(),
+        voice: voice.clone(),
+    };
+    return request;
 }
 
+pub async fn koboldcpp_tts_send_prompt(
+    destination: &String,
+    output_filename: &String,
+    model: &String,
+    input: &String,
+    voice: &String,
+) -> Result<File> {
+    let client = reqwest::Client::new();
+    let request = koboldcpp_tts_build_prompt(model, input, voice);
+    let response = client
+        .post(destination)
+        .json(&request)
+        .send()
+        .await
+        .context("Failed to send request to KoboldCPP")?;
+    //println!("{:?}", response);
+    let mut output_file = File::create(output_filename).context("Failed to create output file")?;
+    let mut stream = response.bytes_stream();
+    while let Some(chunk) = stream.next().await {
+        output_file
+            .write_all(&chunk?)
+            .expect("Failed writing the chunks");
+    }
+    Ok(output_file)
+}
+
+// Just kills a process by its pid
+pub fn process_killer(pid_to_kill: &u32) {
+    // Only get processes, without tasks
+    let sys = System::new_with_specifics(
+        RefreshKind::nothing().with_processes(ProcessRefreshKind::everything().without_tasks()),
+    );
+    // Refresh
+    let koboldcpp_process = match sys.process(sysinfo::Pid::from_u32(*pid_to_kill)) {
+        Some(process) => process,
+        None => panic!("Something went wrong: Koboldcpp PID is wrong."),
+    };
+    match koboldcpp_process.kill_and_wait() {
+        Ok(result) => match result {
+            Some(exit_status) => println!("Koboldcpp exited with status: {}", exit_status),
+            None => panic!("Something happened when trying to wait and kill koboldcpp"),
+        },
+        Err(error) => panic!(
+            "Something went wrong when trying to kill and wait koboldcpp: {}",
+            error
+        ),
+    };
+}
 // TODO: Revisit
 //fn build_prompt(
 //    model: &String,
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use crate::connection::{koboldcpp_start, llama_send_prompt};
+use std::{thread::sleep, time::Duration};
+
+use crate::connection::{
+    koboldcpp_start, koboldcpp_tts_send_prompt, llama_send_prompt, process_killer,
+};
 use clap::Parser;
 
 mod connection;
@@ -10,18 +14,54 @@ mod connection;
     about = "Connect to a local LLM",
     long_about = "Mostly for testing ATM."
 )]
+
+// TODO: Put stuff that only belongs to a certain category under a sub struct?
+// Dunno if that's possible
 struct Cli {
-    /// Directory where models are
+    /// Mode
     #[arg(short, long)]
+    mode: Option<String>,
+    /// Host address
+    #[arg(short = 'A', long)]
+    host: Option<String>,
+    /// Port
+    #[arg(short, long)]
+    port: Option<u32>,
+    /// Directory where models are
+    #[arg(short = 'M', long)]
     model_dir: Option<String>,
     /// Directory where the voice references are
     #[arg(short, long)]
     voice_refs_dir: Option<String>,
+    /// Line to voice
+    #[arg(short, long)]
+    text: Option<String>,
+    /// Output filename
+    #[arg(short, long)]
+    output_filename: Option<String>,
 }
 
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
+    let mode = match cli.mode {
+        Some(string) => string,
+        None => panic!("No mode specified."),
+    };
+    let host = match cli.host {
+        Some(host) => host,
+        None => {
+            println!("No host specified, defaulting to localhost");
+            "localhost".to_string()
+        }
+    };
+    let port = match cli.port {
+        Some(port) => port,
+        None => {
+            println!("No port specified, defaulting to 5001");
+            5001
+        }
+    };
     let model_dir = match cli.model_dir {
         Some(string) => string,
         None => panic!("No model dir specified."),
@@ -30,6 +70,49 @@ async fn main() {
         Some(string) => string,
         None => panic!("No model dir specified."),
     };
-    koboldcpp_start(&"tts".to_string(), &model_dir, &voice_refs_dir);
+    let text = match cli.text {
+        Some(text) => text,
+        None => panic!("No text to voice specified."),
+    };
+    let output_filename = match cli.output_filename {
+        Some(output_filename) => output_filename,
+        None => {
+            println!("Filename not set. Will be saved as 'voiced_file.mp3'");
+            "voiced_file.mp3".to_string()
+        }
+    };
 
+    // match mode.as_str() {
+    //     "tts" => todo!("Add stuff about TTS"),
+    //     "chat" => todo!("Add stuff about chatting"),
+    //     "music" => todo!("Add stuff about music"),
+    //     &_ => todo!("Deal with this"),
+    // }
+    let koboldcpp_pid = match koboldcpp_start(
+        &"tts".to_string(),
+        &host,
+        &port,
+        &model_dir,
+        &voice_refs_dir,
+    )
+    .await
+    {
+        Ok(pid) => pid,
+        Err(_) => panic!("Failed to start koboldcpp"),
+    };
+
+    let koboldtts_result = koboldcpp_tts_send_prompt(
+        &"http://localhost:5001/v1/audio/speech".to_owned(),
+        &output_filename.to_owned(),
+        &"kcpp".to_owned(),
+        &text.to_owned(),
+        &"test qwen 2.mp3".to_owned(),
+    )
+    .await;
+    match koboldtts_result {
+        Ok(_) => (),
+        Err(_) => (),
+    }
+    process_killer(&koboldcpp_pid);
+    println!("Thanks for using llm-connect!");
 }
